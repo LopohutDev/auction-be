19701a1f8a1111160963da5c1fc555b3952dfeb1
Mukul <mukul.k@aveosoft.com>
2022-09-08 09:16:42 +0530

    feat: [AM-16] added location testing for jest

--- src/Modules/admin/location/test/location.controller.spec.ts
+++ src/Modules/admin/location/test/location.controller.spec.ts
@@ -1,18 +1,47 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { LocationController } from 'src/Modules/admin/location/location.controller';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { Test } from '@nestjs/testing';
+import { faker } from '@faker-js/faker';
+import { AppModule } from 'src/app.module';
+import { AdminLocationRoute } from '../../routes/admin.routes';
 
-describe('LocationController', () => {
-  let controller: LocationController;
+describe('Location', () => {
+  let app: INestApplication;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [LocationController],
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
     }).compile();
 
-    controller = module.get<LocationController>(LocationController);
+    app = moduleRef.createNestApplication();
+    await app.init();
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it(`/GET ${AdminLocationRoute}`, () => {
+    return request(app.getHttpServer())
+      .get(AdminLocationRoute)
+      .expect(422)
+      .expect({ statusCode: 422, message: 'Location is required' });
+  });
+
+  it(`/POST ${AdminLocationRoute}`, () => {
+    const values = {
+      city: faker.address.city(),
+      address: faker.address.streetAddress(),
+      Warehouses: Array.from({ length: 3 }).map(() => {
+        return {
+          assletter: faker.random.alpha({ casing: 'upper' }),
+          areaname: faker.random.alpha({ casing: 'upper' }),
+        };
+      }),
+    };
+    return request(app.getHttpServer())
+      .post(AdminLocationRoute)
+      .send(values)
+      .expect(200);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
