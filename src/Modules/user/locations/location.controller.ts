1f4df8f6354e7bb4ebbb1828f91db19c2524d103
richa.d <richa.d@aveosoft.com>
2022-09-27 15:55:48 +0530

    location and item type dropdown api

--- src/Modules/user/locations/location.controller.ts
+++ src/Modules/user/locations/location.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, HttpException } from '@nestjs/common';
+import { Controller, Get, HttpException, Param } from '@nestjs/common';
+import { locationQueryDataDto } from 'src/dto/admin.location.module.dto';
 import { UserLocationRoute } from '../routes/user.routes';
 import { UserLocationService } from './location.service';
 
@@ -10,6 +11,19 @@ export class UserLocationController {
   async getAllLocations() {
     const { success, error, data } =
       await this.locationservice.getAllLocationService();
+    if (success && data) {
+      return {
+        success,
+        data,
+      };
+    } else {
+      throw new HttpException(error.message, error.status);
+    }
+  }
+  @Get(':location')
+  async getLocationsItems(@Param() param: locationQueryDataDto) {
+    const { success, error, data } =
+      await this.locationservice.getLocationItemsService(param.location);
     if (success && data) {
       return {
         success,
--- src/Modules/user/locations/location.service.ts
+++ src/Modules/user/locations/location.service.ts
@@ -17,4 +17,30 @@ export class UserLocationService {
       return { error: { status: 500, message: 'Server error' } };
     }
   }
+  async getLocationItemsService(param: string) {
+    try {
+      const data = await this.prismaService.location.findFirst({
+        where: { locid: param },
+        select: {
+          id: true,
+          Warehouses: {
+            select: {
+              id: true,
+              areaname: true,
+            },
+          },
+          locationItem: {
+            select: {
+              id: true,
+              tagname: true,
+            },
+          },
+        },
+      });
+      return { success: true, data };
+    } catch (error) {
+      this.logger.debug(error?.message || error);
+      return { error: { status: 500, message: 'Server error' } };
+    }
+  }
 }
