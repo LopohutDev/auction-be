bd42bd0c5df4ab6515952b31fc54cd4833e96d9b
Shraddha <shraddha.vaghani@gmail.com>
2022-11-08 13:55:27 +0530

    deleted

--- src/Modules/admin/auction /auction.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuctionController } from './auction.controller';
-import { AuctionService } from './auction.service';
-
-@Module({
-  controllers: [AuctionController],
-  providers: [AuctionService],
-})
-export class AuctionModule {}
